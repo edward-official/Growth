43e21b99029a50144566d47e465ec268c07b16e1
edwardlee <openmoresome@gmail.com>
2025-12-14 14:10:56 +0900

    React + Next

---
+++ nmm-board/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-10">
+      <h1 className="text-4xl font-bold text-purple-600">
+        Next + TS + Tailwind OK
+      </h1>
+    </main>
+  );
+}
